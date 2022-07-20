42127ad9300028ef7367665b3216dacc03681eae
Tyler Ruff <hello@blazed.space>
2022-07-19 23:28:44 -0400

    BLZ

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ $(document).ready(function () {
 		});
 	});
 });
+
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Exit Fullscreen";
+    }
+    else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "Go Fullscreen";
+    }
+});
